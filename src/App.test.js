8598558a6a1162bfa36f8f0a51ede82f8b6b1828
Briana Daniels <briana.daniels@hp.com>
2025-05-19 12:05:40 -0500

    Added Unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -14,45 +14,57 @@ test('App should render', () => {
 });
 
 test('Button should render', () => {
-  // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  render(<App />
+  );
+  const button= screen.getByRole('button', {name:/theme:/i});
+  expect(button).toBeInTheDocument();
 });
 
-/**
- * Verify clicking button should change theme
- * hint: use fireEvent.click(element) to trigger a click event on an element
- */
+// /**
+//  * Verify clicking button should change theme
+//  * hint: use fireEvent.click(element) to trigger a click event on an element
+//  */
 test('theme button should update button text', () => {
-  // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  render(<App />);
+  const button = screen.getByRole('button', { name: /theme:/i });
+  const initialText = button.textContent;
+  fireEvent.click(button);
+  expect(button.textContent).not.toBe(initialText);
 });
 
-// BONUS
-// hint: there is a `.toHaveStyle` method.
-// e.g.: expect(element).toHaveStyle('color: #FFF');
+// // BONUS
+// // hint: there is a `.toHaveStyle` method.
+// // e.g.: expect(element).toHaveStyle('color: #FFF');
+
 test('theme button should toggle styles', () => {
-  // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
-});
-
-/**
- * Verify clicking button should toggle hidden content
- *
- * hint: you can check if something does not exist by using .not
- * e.g. expect(element).not.toBeInTheDocument()
- *
- * hint: use `queryByText` instead of `getByText` to check if something is _not_ rendered
- * (getByText will throw an error if it is not rendered)
- */
-test('hidden button should toggle hidden content', () => {
-  // TODO: change the expect to actually test something 😉
-  expect('no test written').toBe('tested');
+  render(<App />);
+  const themeButton = screen.getByRole('button', {name: /theme:/i });
+   fireEvent.click(themeButton);
+   expect(themeButton).toHaveStyle('color: ButtonText');
 });
 
 
-/**
- * Want more? Try these:
- *   - check for the presence of a specific element, like the paragraph containing the text "Click the button to toggle the theme"
- *   - check the for the class name .container on the surrounding div
- *   - after clicking the toggle hidden content button, check for the button text to update to "hide" instead of "show"
- */
+// /**
+//  * Verify clicking button should toggle hidden content
+//  *
+//  * hint: you can check if something does not exist by using .not
+//  * e.g. expect(element).not.toBeInTheDocument()
+//  *
+//  * hint: use `queryByText` instead of `getByText` to check if something is _not_ rendered
+//  * (getByText will throw an error if it is not rendered)
+//  */
+   test('hidden button should toggle hidden content', () => {
+    render(<App />);
+    const hiddenButton = screen.getByRole('button', {name: /theme:/i });
+    fireEvent.click(hiddenButton);
+
+   expect(hiddenButton).toBeInTheDocument();
+                });
+
+
+// /**
+//  * Want more? Try these:
+//  *   - check for the presence of a specific element, like the paragraph containing the text "Click the button to toggle the theme"
+//  *   - check the for the class name .container on the surrounding div
+//  *   - after clicking the toggle hidden content button, check for the button text to update to "hide" instead of "show"
+//  */
